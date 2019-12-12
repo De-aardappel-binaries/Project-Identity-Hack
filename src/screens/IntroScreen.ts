9366949986abfc768bcb700c6ebb08bd885387be
alex <alex.spelt@breinwave.nl>
2019-12-12 14:52:13 +0100

    plus improve intro screen

--- src/screens/IntroScreen.ts
+++ src/screens/IntroScreen.ts
@@ -7,13 +7,31 @@ class IntroScreen extends GameScreen {
 
     public constructor(game: Game) {
         super(game);
+
+        // set game background for this screen
+        document.getElementById('body').style.backgroundImage = "url('https://ak6.picdn.net/shutterstock/videos/5631416/thumb/1.jpg')";
+        document.getElementById('body').style.backgroundSize = "cover";
+        
+        // create button
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.game.canvas.width / 2 - 100, this.game.canvas.height - 100, 200, 50);
+        ctx.fillRect(this.game.canvas.width / 2 -250, 65, 500, 50);
 
-        ctx.fillText("Hello World", 100, 500); 
-        
-        ctx.rect(this.game.canvas.width / 2 - 100, this.game.canvas.height - 100, 200, 50);
-        ctx.stroke();
+        ctx.textAlign = "center";
+        ctx.font = "30px Arial";
+        ctx.fillStyle = 'black';
+        ctx.fillText("Start game", this.game.canvas.width / 2, this.game.canvas.height - 65); 
+        ctx.fillText("Druk op start om het spel te starten", this.game.canvas.width / 2, 100); 
+    }
+
+    public listen(input: UserInput) {
+        const isPressed = input.GetMousePressed();
+
+        if(isPressed) {
+            console.log(isPressed);
+        }
     }
 }
