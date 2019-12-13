f62e10077551838c97972ab368f264d48e9edd73
laurensjan <brug0015@hz.nl>
2019-12-13 10:05:49 +0100

    Make the webpage responsive on mobile devices.....i hope

--- src/screens/DeepFakeScreen.ts
+++ src/screens/DeepFakeScreen.ts
@@ -3,28 +3,30 @@
 
 class DeepFakeScreen extends GameScreen {
 
-    private origninal:HTMLImageElement;
-    private deepfakeimage:HTMLImageElement
+    private origninal: HTMLImageElement;
+    private deepfakeimage: HTMLImageElement
 
     // private readonly canvas: HTMLCanvasElement;
     // private readonly ctx: CanvasRenderingContext2D;
     public DeepFakeList: Array<Deepfake>
-    public draw(ctx: CanvasRenderingContext2D){
-        this.game.ctx.font = `60px Ariel`;
-        this.game.ctx.fillStyle = "blue";
-        this.game.ctx.textAlign = "center";
-        this.game.ctx.fillText("Zoek de Verschillen", 750, 100);
-
-
-        this.game.ctx.drawImage(this.origninal, 200,150 )
-        this.game.ctx.drawImage(this.deepfakeimage, 800,150 )
+    public draw(ctx: CanvasRenderingContext2D) {
+        // Draw boxes
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.game.canvas.width / 2 - 250, 65, 500, 50);
+        // Draw Text
+        ctx.textAlign = "center";
+        ctx.font = "30px Arial";
+        ctx.fillStyle = 'black';
+        ctx.fillText("Zoek de Verschillen", this.game.canvas.width / 2, 100);
+        this.game.ctx.drawImage(this.origninal, this.game.canvas.width / 4 - this.origninal.width / 2, 150)
+        this.game.ctx.drawImage(this.deepfakeimage, this.game.canvas.width / 4 * 3 - this.deepfakeimage.width / 2, 150)
 
     }
 
-    
 
-    
-    public listen(input: UserInput){}
+
+
+    public listen(input: UserInput) { }
 
     constructor(game: Game) {
         super(game);
@@ -37,11 +39,11 @@ class DeepFakeScreen extends GameScreen {
         imageDeepfake.src = "./assets/images/iWljgsxiCGvQsEPR.jpg"; // deepfakeImage 
         this.deepfakeimage = imageDeepfake
 
-        
+
     }
 
-  
 
 
-    
+
+
 }
