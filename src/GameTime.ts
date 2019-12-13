95b563e01532c8df63cead15ba34b54bc53a197a
alex <alex.spelt@breinwave.nl>
2019-12-13 13:40:20 +0100

    added new stuff to timer

--- src/GameTime.ts
+++ src/GameTime.ts
@@ -1,24 +1,59 @@
 // tslint:disable member-ordering
 
 class GameTime {
-    private readonly canvas: HTMLCanvasElement;
-    private readonly ctx: CanvasRenderingContext2D;
+
     private static time: number;
 
-    private static timer: any;
+    private static timer: number;
 
-    public constructor(time: number){
+    /**
+     * This funtion sets the timer
+     * @param time Amount of seconds
+     */
+    public static setTime(time: number){
         GameTime.time = time;
     }
 
+    /**
+     * Starts timer
+     */
     public static startTimer(){
         GameTime.timer = setInterval(() => GameTime.time--, 1000);
     }
+
+    /**
+     * Stops timer
+     */
     public static stopTimer(){
         clearInterval(GameTime.timer);
     }
+    
+    /**
+     * Returns time in seconds
+     */
+    public static returnTime(): number {
+        return GameTime.time;
+    }
 
-    public static returnTime() {
-        return Math.floor(GameTime.time / 60) + ':' + GameTime.time % 60;
+    /**
+     * Returns time in formated string
+     */
+    public static returnTimeFormatted(): string {
+        let min: number = Math.floor(GameTime.time / 60);
+        let sec: number | string = GameTime.time % 60
+
+        // convert 3:5 to 3:05 
+        if(sec < 10) 
+            sec = "0" + sec;
+
+        return `${min}:${sec}`;
+    }
+
+    /**
+     * Removes time of timer
+     * @param time Amount of seconds
+     */
+    public static removeTime(time: number) {
+        GameTime.time = GameTime.time - time;
     }
 }
