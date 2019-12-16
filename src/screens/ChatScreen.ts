bbaf96feabc53fe61d85d8516b6361d9ef8919d2
alex <alex.spelt@breinwave.nl>
2019-12-16 15:02:44 +0100

    ChatScreen begin

--- src/screens/ChatScreen.ts
+++ src/screens/ChatScreen.ts
@@ -2,5 +2,28 @@
 /// <reference path="../GameScreen.ts"/>
 
 class ChatScreen extends GameScreen {
+
+    private Chats: Array<Array<ChatMessage>>;
     
+    constructor(game: Game) {
+        super(game);
+    }
+    
+    public draw(ctx: CanvasRenderingContext2D) {
+        // Draw background screen
+        ctx.fillStyle = 'white';
+        ctx.fillRect(
+            this.game.canvas.width * 0.1,
+            0, 
+            this.game.canvas.width * 0.8, 
+            this.game.canvas.height
+        );
+        ctx.fillStyle = '';
+    }
+
+}
+
+interface ChatMessage {
+    message: string,
+    self: boolean
 }
