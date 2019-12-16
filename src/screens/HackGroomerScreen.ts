d86b8c2de936a35663fdf908e2238b8ce3130029
alex <alex.spelt@breinwave.nl>
2019-12-16 15:41:53 +0100

    grid for hackergroomer screen has been rewritten

--- src/screens/HackGroomerScreen.ts
+++ src/screens/HackGroomerScreen.ts
@@ -2,86 +2,62 @@
 /// <reference path="../GameScreen.ts"/>
 
 class HackGroomerScreen extends GameScreen{
-    private symbolButton: UIButton;
+    
+    private readonly grid: number = 10;
+    private readonly buttonSize: number = 50;
+
+
+    private abc = 'abcdefghijklmnopqrstuvwxyz';
+    private startPoint: Pos;
+
+    constructor(game: Game) {
+        super(game);
+
+        this.startPoint = {
+            xPos: (game.canvas.width - (this.buttonSize * this.grid)) / 2, 
+            yPos: 200
+        }
+
+        // randomize hier de abc string
+    }
 
     public draw(ctx: CanvasRenderingContext2D) {
-        console.log(GameTime.returnTime());
-        this.drawGrid(ctx);
-        // Draw title box
-        ctx.fillStyle = 'white';
-        ctx.fillRect(this.game.canvas.width / 2 - 250, 65, 500, 50);
-        // Draw Text
-        ctx.textAlign = "center";
-        ctx.font = "30px Arial";
-        ctx.fillStyle = 'black';
-        ctx.fillText("Elimineer ieder symbool", this.game.canvas.width / 2, 100);
-        // Draw symbols
-    }
+        let count = 0;
 
-    private drawGrid(ctx: CanvasRenderingContext2D){
-        let text = "B";
-        // Draw grid
-        for (let x = 0; x < 6; x++) {
-            for (let y = 0; y < 6; y++) {  
+        for(let y = 0; y < this.grid; y++) {
+            for(let x = 0; x < this.grid; x++) {
                 ctx.fillStyle = 'white';
-                ctx.fillRect((this.game.canvas.width / 2) - x*50, (this.game.canvas.height / 2) - y*50, 50, 50);
-                ctx.strokeStyle = 'red';
-                ctx.lineWidth = 5;
-                ctx.strokeRect((this.game.canvas.width / 2) - x*50, (this.game.canvas.height / 2) - y*50, 50, 50);
-                ctx.textAlign = "center";
-                ctx.font = "40px Arial";
-                ctx.fillStyle = 'blue';
-                ctx.textBaseline = "middle";
-                this.fillTextWithSpacing(ctx, text, (this.game.canvas.width / 2) - x*42, (this.game.canvas.height / 2) - y*42, 10);
-                //ctx.fillText("A", (this.game.canvas.width / 2) - x*42, (this.game.canvas.height / 2) - y*42);
+                ctx.strokeStyle = 'black';
+                ctx.fillRect(
+                    this.startPoint.xPos + (x * this.buttonSize), 
+                    this.startPoint.yPos + (y * this.buttonSize),
+                    this.buttonSize,
+                    this.buttonSize
+                );
+                ctx.strokeRect(
+                    this.startPoint.xPos + (x * this.buttonSize), 
+                    this.startPoint.yPos + (y * this.buttonSize),
+                    this.buttonSize,
+                    this.buttonSize
+                );
+                ctx.fillStyle = 'black';
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'middle';
+                ctx.fillText(
+                    this.abc[count],
+                    this.startPoint.xPos + (x * this.buttonSize) + (this.buttonSize/2), 
+                    this.startPoint.yPos + (y * this.buttonSize) + (this.buttonSize/2)
+                );
+
+                if(this.abc.length-1 == count)
+                    count = 0;
+                else
+                    count++;
             }
         }
     }
-
-    private fillTextWithSpacing(ctx: CanvasRenderingContext2D, text: string, x: number, y: number, spacing: number){
-        //Start at position (X, Y).
-        //Measure wAll, the width of the entire string using measureText()
-        let wAll = ctx.measureText(text).width;
-            do{
-            //Remove the first character from the string
-            let char = text.substr(0, 1);
-            let wShorter = null;
-            text = text.substr(1);
-            //Print the first character at position (X, Y) using fillText()
-            ctx.fillText(char, x, y);
-            //Measure wShorter, the width of the resulting shorter string using measureText().
-                if (text === "")
-                    wShorter = 0;
-                else
-                    wShorter = ctx.measureText(text).width;
-            //Subtract the width of the shorter string from the width of the entire string, giving the kerned width of the character, wChar = wAll - wShorter
-            let wChar = wAll - wShorter;
-            //Increment X by wChar + spacing
-            x += wChar + spacing;
-            //wAll = wShorter
-            wAll = wShorter;
-            //Repeat from step 3
-            }
-        while (text != "");
-    }
-
 
     public listen(input: UserInput) {
-        const isPressed = input.GetMousePressed();
-
-        if(isPressed) {
-            if(this.symbolButton.checkIfPressed(isPressed)) {
-                GameTime.startTimer();
-            }
-        }
-    }
-
-    /**
-     * Renders a random number between min and max
-     * @param {number} min - minimal time
-     * @param {number} max - maximal time
-     */
-    private randomNumber(min: number, max: number): number {
-        return Math.round(Math.random() * (max - min) + min);
+        
     }
 }
